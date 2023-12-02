61d45b29454de81d317094d214cd25a13ac788d8
Vasilyev, Artem <artem001v@gmail.com>
2023-12-02 04:01:42 +0300

    Installed z and fixed shortcut paths

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
-function goToWork {set-location"~\Desktop\Work\development\"}
-function goToEmp {set-location"~\Desktop\Work\development\EMP\Client"}
-function goToEmp2 {set-location"~\Desktop\Work\development\EMP_Develop\Client"}
-function goToVim {set-location"~\AppData\Local\nvim"}
+function goToWork {set-location "~\Desktop\Work\development\"}
+function goToEmp {set-location "~\Desktop\Work\development\EMP\Client"}
+function goToEmp2 {set-location "~\Desktop\Work\development\EMP_Develop\Client"}
+function goToVim {set-location "~\AppData\Local\nvim"}
 
 Set-Alias work goToWork
 Set-Alias emp goToEmp
@@ -9,3 +9,14 @@ Set-Alias emp2 goToEmp2
 Set-Alias cfg goToVim
 Set-Alias lz lazygit
 Remove-Item Alias:curl
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PsFzfOption -EnableAliasFuzzyEdit
+Set-PsFzfOption -EnableAliasFuzzyFasd
+Set-PsFzfOption -EnableAliasFuzzyHistory
+Set-PsFzfOption -EnableAliasFuzzyKillProcess
+Set-PsFzfOption -EnableAliasFuzzySetLocation
+Set-PsFzfOption -EnableAliasFuzzySetEverything
+Set-PsFzfOption -EnableAliasFuzzyScoop
+Set-PsFzfOption -EnableAliasFuzzyZLocation
+Set-PsFzfOption -EnableAliasFuzzyGitStatus
+Set-PsFzfOption -EnableFd
