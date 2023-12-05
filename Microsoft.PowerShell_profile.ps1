1ffb19180a1a6d75d82ee3bbe37f1a5f44d9726f
Vasilyev, Artem <artem001v@gmail.com>
2023-12-05 18:14:18 +0300

    fzf to repo directories

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,19 @@ function goToEmp {set-location "~\Desktop\Work\development\EMP\Client"}
 function goToEmp2 {set-location "~\Desktop\Work\development\EMP_Develop\Client"}
 function goToVim {set-location "~\AppData\Local\nvim"}
 
+function searchWorkDirectory {
+    Get-ChildItem -Path "~\Desktop\Work\development\" | Select FullName | fzf | %{ set-location $_.Trim() }
+    ls
+}
+Set-Alias fwd searchWorkDirectory
+
+function searchPersonalDirectory {
+    Get-ChildItem -Path "~\Desktop\Projects\" | Select FullName | fzf | %{ set-location $_.Trim() }
+    ls
+}
+Set-Alias fpd searchPersonalDirectory
+
+Set-Alias pws powershell.exe
 Set-Alias work goToWork
 Set-Alias emp goToEmp
 Set-Alias emp2 goToEmp2
@@ -20,3 +33,6 @@ Set-PsFzfOption -EnableAliasFuzzyScoop
 Set-PsFzfOption -EnableAliasFuzzyZLocation
 Set-PsFzfOption -EnableAliasFuzzyGitStatus
 Set-PsFzfOption -EnableFd
+
+# Store previous command's output in $__
+$PSDefaultParameterValues['Out-Default:OutVariable'] = '__'
