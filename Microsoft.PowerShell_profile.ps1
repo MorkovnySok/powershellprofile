2c74f5d99a114c19fb9cc4b81076bcf309f1aec2
Artem Vasilyev <artem001v@gmail.com>
2024-05-08 21:58:35 +0300

    added some cool psreadline stuff

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -82,14 +82,62 @@ Set-PsFzfOption -EnableAliasFuzzyGitStatus
 Set-PsFzfOption -EnableFd
 Set-PsFzfOption -TabExpansion
 
+
+# PSReadLine stuff
+# https://raw.githubusercontent.com/PowerShell/PSReadLine/master/PSReadLine/SamplePSReadLineProfile.ps1
 Import-Module PSReadLine
 Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+Set-PSReadLineOption -PredictionViewStyle ListView
 #Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 #Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineOption -EditMode Windows
 
+# This example will replace any aliases on the command line with the resolved commands.
+Set-PSReadLineKeyHandler -Key "Alt+%" `
+                         -BriefDescription ExpandAliases `
+                         -LongDescription "Replace all aliases with the full command" `
+                         -ScriptBlock {
+    param($key, $arg)
+
+    $ast = $null
+    $tokens = $null
+    $errors = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
+
+    $startAdjustment = 0
+    foreach ($token in $tokens)
+    {
+        if ($token.TokenFlags -band [TokenFlags]::CommandName)
+        {
+            $alias = $ExecutionContext.InvokeCommand.GetCommand($token.Extent.Text, 'Alias')
+            if ($alias -ne $null)
+            {
+                $resolvedCommand = $alias.ResolvedCommandName
+                if ($resolvedCommand -ne $null)
+                {
+                    $extent = $token.Extent
+                    $length = $extent.EndOffset - $extent.StartOffset
+                    [Microsoft.PowerShell.PSConsoleReadLine]::Replace(
+                        $extent.StartOffset + $startAdjustment,
+                        $length,
+                        $resolvedCommand)
+
+                    # Our copy of the tokens won't have been updated, so we need to
+                    # adjust by the difference in length
+                    $startAdjustment += ($resolvedCommand.Length - $length)
+                }
+            }
+        }
+    }
+}
+
+
+
 # Store previous command's output in $__
 $PSDefaultParameterValues['Out-Default:OutVariable'] = '__'
+
+
 
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
